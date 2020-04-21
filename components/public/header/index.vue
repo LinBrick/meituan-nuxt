8ed89ede4ebda8c603a197f1013bd9c56def333e
lwy-lenovo <527563266@qq.com>
2020-04-21 22:00:29 +0800

    import fonticon

---
+++ components/public/header/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header-bar">
+    <div class="header-content clearfix">
+      <geo />
+    </div>
+  </div>
+  <!-- <el-row :gutter="20" class="com-header">
+    <el-col :span="6">
+      <geo />
+    </el-col>
+  </el-row> -->
+</template>
+
+<script>
+import Geo from './geo'
+export default {
+  components: {
+    Geo
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,8 @@ module.exports = {
   ** Global CSS
   */
   css: [
-    'element-ui/lib/theme-chalk/index.css'
+    'element-ui/lib/theme-chalk/index.css',
+    '@/assets/meituan-index.scss'
   ],
   /*
   ** Plugins to load before mounting the App
