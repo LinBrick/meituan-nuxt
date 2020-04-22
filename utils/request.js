94c4aa939f7db6f93a4500edcc6b5f9801f04459
linwenyao <linwenyao@flytexpress.com>
2020-04-22 18:18:52 +0800

    mock数据

---
+++ utils/request.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+const service = axios.create({
+  baseURL: '"http://localhost:3000"', // api 的 base_url
+  timeout: 5000 // request timeout
+})
+
+export default service
