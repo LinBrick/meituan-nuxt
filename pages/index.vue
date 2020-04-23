9f2e19c9668cb81d094b8a221c012bde70f6ea6a
linwenyao <linwenyao@flytexpress.com>
2020-04-23 16:47:46 +0800

    添加电影推荐组件

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,19 @@
   <div class="page index">
     <div class="index-container">
       <my-banner />
+      <movie-recommend />
     </div>
   </div>
 </template>
 
 <script>
 import MyBanner from '@/components/home/banner/index'
+import MovieRecommend from '@/components/home/movieRecommend/index'
 
 export default {
   components: {
-    MyBanner
+    MyBanner,
+    MovieRecommend
   }
 }
 </script>
